46ed6e1721a846eba76f52f369eeb3a00c730fee
Diego Centelles <dieguscentbelt@gmail.com>
2015-04-02 00:19:51 +0200

    corregidos fichero de compilación

--- Release/src/Checksum.d
+++
@@ -1,7 +0,0 @@
-src/Checksum.d: ../src/Checksum.cpp \
- /home/diego/Git/ROVRadioTransmission/git/ROVRadioTransmission/includes/Checksum.h \
- /home/diego/Git/ROVRadioTransmission/git/ROVRadioTransmission/includes/customtypes.h
-
-/home/diego/Git/ROVRadioTransmission/git/ROVRadioTransmission/includes/Checksum.h:
-
-/home/diego/Git/ROVRadioTransmission/git/ROVRadioTransmission/includes/customtypes.h:
