2c9edd34d2f80458544317ec685e22eb9324ea16
Diego Centelles <dieguscentbelt@gmail.com>
2015-10-06 20:56:14 +0200

    ahora se puede especificar la resolucion de la imagen en la recepcion

--- ImageTransmission/videoRovRx.sh
+++ ImageTransmission/videoRovRx.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-#if [ $# -lt 1 ]
-#then
-#	echo "Número de argumentos incorrecto: <EncodedImageSize>" >&2
-#	exit 1
-#fi
-#
-#imsize=$1
+if [ $# -lt 2 ]
+then
+	echo "Número de argumentos incorrecto: <ImageWidth> <ImageHeight>" >&2
+	exit 1
+fi
+
+width=$1
+heigth=$2
 
 
-./bin/RX imagen 90000 | ./bin/debter -d -I imagen | ffmpeg -f rawvideo -s 1280x720 -i pipe:0  -f rawvideo -pix_fmt yuv420p pipe:1 | ffplay -f rawvideo -s 1280x720 -i pipe:0 -pix_fmt yuv420p -window_title "OpenROV Camera"
+./bin/RX imagen 90000 | ./bin/debter -d -I imagen | ffmpeg -f rawvideo -s $1x$2 -i pipe:0  -f rawvideo -pix_fmt yuv420p pipe:1 | ffplay -f rawvideo -s $1x$2 -i pipe:0 -pix_fmt yuv420p -window_title "OpenROV Camera"
 
 
