5baeec21a443ca7e1f123b07faceceb27718306f
Diego Centelles <al132071@uji.es>
2015-10-06 17:56:43 +0000

    Anyadido script para compilar en BBB; + version del transmisor cuando no hay Arduino de por medio

--- includes/Radio.h
+++ includes/Radio.h
@@ -8,7 +8,6 @@
 #ifndef RADIO_H_
 #define RADIO_H_
 
-#include <Arduino.h>
 #include <SerialPortInterface.h>
 #include <DataLinkFrame.h>
 
@@ -25,7 +24,7 @@ namespace radiotransmission
 		};
 
 		~Radio();
-		Radio(unsigned char dir, Arduino &, Radio::fcsType fcst = fcsType::crc32, uint32_t maxRxBufferSize = 5000);
+		Radio(unsigned char dir, SerialPortInterface &, Radio::fcsType fcst = fcsType::crc32, uint32_t maxRxBufferSize = 5000);
 		void SendBytes(const void *, uint32_t size, uint8_t dirTo = 255, uint32_t packetSize=1000, unsigned long ms=0);
 		void ReceiveBytes(void *, uint32_t size, uint8_t dirFrom = 255, unsigned long ms=10000);
 
@@ -36,7 +35,7 @@ namespace radiotransmission
 		uint32_t _rxBufferLastPos;
 		uint32_t _bytesInBuffer = 0;
 		unsigned char dir;
-		Arduino & arduino;
+		SerialPortInterface & serial;
 		Radio::fcsType FCSType;
 
 		void _DecreaseBytesInBuffer();
