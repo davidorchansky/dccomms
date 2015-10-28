31bfadbf4156de5a9cc0e3c877b94873688e6fb5
Diego Centelles <dieguscentbelt@gmail.com>
2015-10-28 02:32:21 +0100

    Primera version buena del codigo de Arduino para los nRF24l01, usando la funcion de auto ack (Enhanced ShockBurst TM)

--- src/Arduino/radio_rx_UNO_nR24l01_v1/nRF24l01_RX_v1.ino
+++ src/Arduino/radio_rx_UNO_nR24l01_v1/nRF24l01_RX_v1.ino
@@ -59,8 +59,8 @@ unsigned char TX_ADDRESS[TX_ADR_WIDTH]  =
   0x34,0x43,0x10,0x10,0x01
 }; // Define a static TX address
 
-unsigned char rx_buf[TX_PLOAD_WIDTH];
-unsigned char tx_buf[TX_PLOAD_WIDTH];
+//unsigned char rx_buf[TX_PLOAD_WIDTH];
+//unsigned char tx_buf[TX_PLOAD_WIDTH];
 //***************************************************
 
 //unsigned char g_status;
@@ -69,6 +69,7 @@ char caracterActual = -1;
 
 void mycopy(void * dst, void * orig, unsigned int tam)
 {
+  //Podriamos usar memmove, o memcpy en la mayoria de casos
   uint8_t * dptr, *optr;
   dptr = (uint8_t*) dst;
   optr = (uint8_t*) orig;
@@ -86,84 +87,23 @@ class Radio
   public:
     Radio()
     {
-       _available = 0;
     }
     ~Radio(){};
 
     int Available()
     {
-       if (_available)
-         return _available;
-       
-       return _dataReady();
-    }
-
-    
-    void ReadBytes(void * dst, unsigned int req) //Blocking call
-    {
-      //Serial.print("QUIERO LEER: "); Serial.println(req);
-       if(req <= _available)
-       {
-         memcpy(dst, _buff, req);
-         _available = _available - req;
-         memcpy(_buff, _buff+req, _available);
-        //Serial.print("Leidos todos del buffer: ");Serial.println(req);
-         return;
-       }
-       else
-       {
-         uint8_t * ptr = (uint8_t *) dst;
-         memcpy(ptr, _buff, _available);
-         ptr += _available;
-        // Serial.print("Leidos del buffer: ");Serial.println(_available);
-         unsigned int left = req - _available;
-         if(_available > 0)
-         {
-           while(!_dataReady());
-         }
-         _available = 0;
-         while(left > 0)
-         {
-             uint8_t dataLength;
-             dataLength = SPI_Read(0x60);
-             SPI_Read_Buf(RD_RX_PLOAD, _buff, dataLength);
-             SPI_RW_Reg(FLUSH_RX,0);
-             
-             if(dataLength > TX_PLOAD_WIDTH) continue;
-             //Serial.print("Recibidos: "); Serial.println(dataLength);
-             if(dataLength <= left)
-             {
-               memcpy(ptr, _buff, dataLength);
-               ptr += dataLength;
-               left -= dataLength;
-               //Serial.print("Leidos: ");Serial.println(dataLength);
-             }
-             else
-             {
-               memcpy(ptr, _buff, left);
-               _available = dataLength - left;
-               memcpy(_buff, _buff+left, _available);
-              // Serial.print("Leidos: ");Serial.println(left);
-              // Serial.print("Dejo en buffer: ");Serial.println(_available);
-               left = 0;
-           }
-           if(left > 0)
-             while(!this->Available());
-           
-          // Serial.print("Left: ");Serial.println(left);
-           
-         }
-       }
-        
+       return _readyForRead();
     }
     private:
-       uint8_t _buff[TX_PLOAD_WIDTH];
-       uint8_t _available = 0;
        uint8_t _status;
-       int _dataReady()
+       uint8_t _fifostatus;
+       
+       int _readyForRead()
        {
              _status = SPI_Read(STATUS);
-             boolean result =  (_status)&RX_DR;
+             _fifostatus = SPI_Read(FIFO_STATUS); 
+             boolean result =  ((_status&RX_DR) || !(_fifostatus&RX_EMPTY)) && (_fifostatus&TX_EMPTY);
+             
              SPI_RW_Reg(WRITE_REG+STATUS,_status);
              return result;
        }
@@ -189,49 +129,6 @@ void readNBytes(char* buff, int tam, Stream * stream)
 }
 
 
-uint16_t radioFrameReceived(char* buffer, unsigned char* pre, unsigned char* maxPrePos, unsigned char **preamblePos)
-{
-  if (*preamblePos == maxPrePos)
-  {
-    //Serial.println("Preambulo recibido");
-    *preamblePos = pre;
-    radio.ReadBytes(buffer, INFO_SIZE);
-    uint16_t dsize;
-    uint16_t *fdsize = (uint16_t *)(buffer+2);
-    if(BigEndian)
-    {
-      dsize = *fdsize;
-    }
-    else
-    {
-      dsize = ((*fdsize) << 8) | ((*fdsize) >> 8);
-    }
-    if(dsize <= BUFFER_SIZE-FCS_SIZE)
-    {
-      
-      radio.ReadBytes(buffer+INFO_SIZE, dsize + FCS_SIZE);
-      return dsize;
-    }
-    //Serial.println("ERROR");
-    return 0;
-  }
-
-  char car;
-  
-  radio.ReadBytes(&car,1); 
-  //Serial.print("leido: ");Serial.println(car);  
-  if (car == **preamblePos)
-  {
-    (*preamblePos)++;
-  }
-  else
-  {
-   
-    *preamblePos = pre;
-  }
-
-  return 0;
-}
 
 boolean commandReceived(Stream * s, char* buffer, int buffSize, unsigned char* pre, unsigned char* maxPrePos, unsigned char **preamblePos)
 {
@@ -287,83 +184,20 @@ int paylsize;
 
 void loop() 
 {
-   
-  /*** TEST 2 ***/
-  /*
-  unsigned char auxbuf[5];
-  int reqs = 5;//TX_PLOAD_WIDTH;
-  unsigned int  dataLength;
-  for(;;)
-  {
-    if(radio.Available())
-    {
-  
-      radio.ReadBytes(auxbuf,reqs);
-      Serial.println("###########");
-      for(int i=0; i<reqs; i++)
-      {
-          Serial.print(" ");
-         // Serial.print((char)auxbuf[i]);                              // print rx_buf
-          Serial.print(auxbuf[i], HEX);                              // print rx_buf
-      }
-      
-    }
-    else if (Serial.available() > 0)
-    {
-      caracterActual = Serial.peek();
-  
-      
-      if (commandReceived(&Serial, NULL, 0, (unsigned char*)areyou, MaxAreYouPos, &serialAreYouPos))
-        Serial.write(imfrom, IMFROM_SIZE);
-  
-      Serial.read();
-      
-    }
-  }
-  */
-  
-  
-  /*** TEST ***/
-  /*
-    for(;;)
+  if (radio.Available())
   {
     uint8_t dataLength;
     unsigned char status = SPI_Read(STATUS);                         // read register STATUS's value
-    if(status&RX_DR)                                                 // if receive data ready (TX_DS) interrupt
+    unsigned char fifostatus = SPI_Read(FIFO_STATUS);
+  
+    if( ((status&RX_DR) || !(fifostatus&RX_EMPTY)) && (fifostatus&TX_EMPTY) )                                                 // if receive data ready (TX_DS) interrupt
     {
-      dataLength = SPI_Read(0x60); //R_RX_PL_WID register. Contiene el width del payload del mensaje recibido
-      Serial.print("Data length: "); Serial.println(dataLength);
-      SPI_Read_Buf(RD_RX_PLOAD, rx_buf, dataLength);             // read playload to rx_buf
-      SPI_RW_Reg(FLUSH_RX,0);                                        // clear RX_FIFO
-      for(int i=0; i<dataLength; i++)
-      {
-          //Serial.print(" ");
-          //Serial.print(rx_buf[i],HEX);                              // print rx_buf
-      }
-      Serial.println(" ");
+      dataLength = SPI_Read(0x60); 
+      SPI_Read_Buf(RD_RX_PLOAD, (unsigned char*)buf, dataLength); 
+      Serial.write(buf, dataLength);
       
-      for(int i=0; i<dataLength; i++)
-      {
-          Serial.print(" ");
-          Serial.print((char)rx_buf[i]);                              // print rx_buf
-      }
-      Serial.println(" ");
     }
-    SPI_RW_Reg(WRITE_REG+STATUS,status);                             // clear RX_DR or TX_DS or MAX_RT interrupt flag
-   // delay(1000);
-  }
-  */
-  /*** FIN TEST ***/
-  if (radio.Available())
-  {
-    //Serial.println("Datos disponibles");
-    if (paylsize=radioFrameReceived(buf, (unsigned char*)preamble, MaxPrePos, &serialPreamblePos))
-    {
-        //Serial.println("ENVIANDO!");
-        Serial.write(preamble, PREAMBLE_SIZE);   
-        Serial.write(buf, INFO_SIZE + paylsize + FCS_SIZE);
-        Serial.flush();
-    }
+    SPI_RW_Reg(WRITE_REG+STATUS,status);
    
   }
   else if (Serial.available() > 0)
@@ -523,7 +357,7 @@ void RX_Mode(void)
 {
   digitalWrite(CE, 0);
   SPI_Write_Buf(WRITE_REG + RX_ADDR_P0, TX_ADDRESS, TX_ADR_WIDTH); // Use the same address on the RX device as the TX device
-  SPI_RW_Reg(WRITE_REG + EN_AA, 0x01);      // Disable Auto.Ack:Pipe0
+  SPI_RW_Reg(WRITE_REG + EN_AA, 0x01);      // Enable Auto.Ack:Pipe0
   //SPI_RW_Reg(WRITE_REG + EN_AA, 0x00);      // Disable Auto.Ack:PipeX
   SPI_RW_Reg(WRITE_REG + EN_RXADDR, 0x01);  // Enable Pipe0
   SPI_RW_Reg(WRITE_REG + RF_CH, 40);        // Select RF channel 40
@@ -537,111 +371,7 @@ void RX_Mode(void)
   
   //SPI_RW_Reg(WRITE_REG + SETUP_RETR, 0x0A);
   digitalWrite(CE, 1);                             // Set CE pin high to enable RX device
-  //  This device is now ready to receive one packet of 16 unsigned chars payload from a TX device sending to address
-  //  '3443101001', with auto acknowledgment, retransmit count of 10, RF channel 40 and datarate = 2Mbps.
+  
 }
 /**************************************************/
 
-/*
-  for(;;)
-  {
-    uint8_t dataLength;
-    unsigned char status = SPI_Read(STATUS);                         // read register STATUS's value
-    if(status&RX_DR)                                                 // if receive data ready (TX_DS) interrupt
-    {
-      dataLength = SPI_Read(0x60); //R_RX_PL_WID register. Contiene el width del payload del mensaje recibido
-      Serial.print("Data length: "); Serial.println(dataLength);
-      SPI_Read_Buf(RD_RX_PLOAD, rx_buf, dataLength);             // read playload to rx_buf
-      SPI_RW_Reg(FLUSH_RX,0);                                        // clear RX_FIFO
-      for(int i=0; i<dataLength; i++)
-      {
-          Serial.print(" ");
-          Serial.print(rx_buf[i],HEX);                              // print rx_buf
-      }
-      Serial.println(" ");
-    }
-    SPI_RW_Reg(WRITE_REG+STATUS,status);                             // clear RX_DR or TX_DS or MAX_RT interrupt flag
-   // delay(1000);
-  }
-*/
-
-/*
-
-class Radio
-{
-  public:
-    Radio(){}
-    ~Radio(){};
-
-    int Available()
-    {
-       g_status = SPI_Read(STATUS);
-       boolean result =  (g_status)&RX_DR;
-       //SPI_RW_Reg(WRITE_REG+STATUS,g_status);                             // clear RX_DR or TX_DS or MAX_RT interrupt flag
-       if(!result)
-       {
-       //Serial.print("Hay datos disponibles: ");
-       //result ? Serial.println("SI"): Serial.println("NO");
-       }
-       return result;
-    }    
-
-    void ReadBytes(void * b, unsigned int t) //Blocking call
-    {
-       // Serial.print("Leer ");Serial.println(t);
-       // Serial.print("BiB ");Serial.println(_bytesInBuffer);
-        if(_bytesInBuffer < t)
-        {
-          memcpy(b, _buff, _bytesInBuffer);
-        }
-        else
-        {
-          memcpy(b, _buff, t);
-          _bytesInBuffer -= t;
-          memcpy(_buff, _buff+t, _bytesInBuffer);
-          return;
-        }
-    
-        uint8_t * ptr = ((uint8_t*) b) + _bytesInBuffer;
-        int bytesLeft = t - _bytesInBuffer;
-        _bytesInBuffer = 0;
-        
-        
-        while(bytesLeft > 0)
-        {
-            //int len = TX_PLOAD_WIDTH < bytesLeft ? TX_PLOAD_WIDTH : bytesLeft;
-            int bytesRead = _ReadUnit(ptr, bytesLeft);
-          //  Serial.print("Ledidos ");Serial.print(bytesRead);Serial.println(" bytes");
-            ptr += bytesRead;
-            bytesLeft -= bytesRead;
-        }
-        
-    }
-    private:
-      uint8_t _buff[TX_PLOAD_WIDTH];
-      int _bytesInBuffer = 0;
-      int _ReadUnit(void * b, uint8_t left) //Blocking call
-      {
-        while(!Available());
-        uint8_t dataLength = SPI_Read(0x60); //R_RX_PL_WID register. Contiene el width del payload del mensaje recibido
-       // Serial.print("Recibidos ");Serial.println(dataLength);
-        SPI_Read_Buf(RD_RX_PLOAD, _buff, dataLength); // read playload to rx_buf
-        SPI_RW_Reg(FLUSH_RX,0);
-
-        //SPI_RW_Reg(WRITE_REG+STATUS,g_status);
-        if(dataLength > left)
-        {
-          memcpy(b, _buff, left);
-          _bytesInBuffer = dataLength-left;
-          memcpy(_buff, _buff+left, _bytesInBuffer);
-          Serial.write((uint8_t *) b, left);
-          return left;
-        }
-        memcpy(b, _buff, dataLength);
-        Serial.write((uint8_t *) b, dataLength);
-        return dataLength;
-      }
-    
-    
-};
-*/
