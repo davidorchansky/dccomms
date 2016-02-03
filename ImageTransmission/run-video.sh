3ea5bb88a09a84334405f9ac24727f7751a9b8d7
Diego Centelles <dieguscentbelt@gmail.com>
2016-02-03 17:29:18 +0100

    Anadido test de envio de 100 imagenes y alguna modificacion de configuracion

--- ImageTransmission/run-video.sh
+++ ImageTransmission/run-video.sh
@@ -15,9 +15,17 @@ sleep 10
 
 #./bin/videoServer_2procOnly 1280 720 430 1500 0
 
-#./bin/TX_allInOne_noServer -W 1280 -H 720 -I imagen -F 3000 -L 1450 -D 800 -A 1000
+#./bin/TX_allInOne_noServer -W 1280 -H 720 -I imagen -F 15000 -L 1450 -D 1500 -A 1000
 
 #./bin/TX_allInOne_noServer -W 1280 -H 720 -I imagen -F 430 -L 1450 -D 0 -A 1000
 
-./bin/TX_allInOne_noServer -W 1280 -H 720 -I imagen -F 1430 -L 1480 -D 0 -A 1000
+#./bin/TX_allInOne_noServer -W 1280 -H 720 -I imagen -F 1440 -L 1480 -D 0 -A 1000
+
+#./bin/TX_allInOne_noServer -W 1280 -H 720 -I imagen -F 1440 -L 1480 -D 0 -A 1000
+
+#./bin/TX_allInOne_noServer -W 1280 -H 720 -I imagen -F 200 -L 1480 -D 10000 -A 1000
+
+
+./bin/TX_allInOne_noServer -W 1280 -H 720 -I imagen -F 500 -L 1480 -D 0 -A 1000
+
 cd -
