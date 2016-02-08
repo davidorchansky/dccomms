2e25c90c4ad023544d0e0839cd2e616bb3282fef
Diego Centelles <dieguscentbelt@gmail.com>
2016-02-08 13:49:39 +0100

    Aplicando resaltado de bordes

--- ImageProc/Canny.c
+++ ImageProc/Canny.c
@@ -9,6 +9,7 @@
 #include <math.h>
 #include <getopt.h>
 #include <errno.h>
+#include <limits.h>
 
 static char *
 pgmName(char *path)
@@ -193,7 +194,216 @@ void getGaussianFilter(float * filtro, unsigned int tam, float sigma)
 
 }
 
-void filtrarRuido(uint8_t * src, uint8_t * dst, unsigned int width, unsigned int height, float * filtro, unsigned int tamFiltro)
+void aplicarFiltro_Uint8_ConPrecision(uint8_t * src, uint8_t * dst, unsigned int width, unsigned int height, float * hfiltro, float * vfiltro, unsigned int tamFiltro)
+{
+	unsigned int size = height * width;
+	
+	double * bufferDeTrabajo = (double *) malloc(size * sizeof(double));
+	//Copiamos todo en bufferDeTrabajo para trabajar con mayor precision
+	int pos;
+	for(pos = 0; pos < size; pos++)
+	{
+		bufferDeTrabajo[pos] = (double) src[pos];
+	}
+
+	int foffset = tamFiltro >> 1;
+	int ioffset = foffset << 1;
+
+
+	//Numero de filas y columnas de la zona central (descartamos bordes de la imagen del grosor del filtro)
+	int filas    = height - ioffset;
+	int columnas = width - ioffset;
+
+	
+	//Pasada por filas (aplicación del filtro horizontal)
+	unsigned int offsetInicial = foffset * width + foffset;
+
+	//uint8_t * dptr;
+	double * dptr;
+
+	//dptr = dst + offsetInicial;
+	dptr = bufferDeTrabajo + offsetInicial;
+
+	//uint8_t * maxf = dptr + width * filas;
+	double * maxf = dptr + width * filas;
+	
+
+	float * centroFiltro = hfiltro + foffset;
+
+#ifdef DEBUG
+	fprintf(stderr, "Aplicando filtro horizontal...\n");
+	double valorMaximo = -9999, valorMinimo = 9999;
+#endif
+	while(dptr < maxf)
+	{
+		//uint8_t * maxc = dptr + columnas;
+		double * maxc = dptr + columnas;
+		while(dptr < maxc)
+		{
+			//Aplicamos el filtro horizontalmente
+			//uint8_t valorFinal = 0;
+			double valorFinal = 0;
+			int idx;
+			for(idx = -foffset; idx <= foffset; idx++)
+			{
+				valorFinal += *(dptr+idx) * *(centroFiltro+idx);
+			}
+			*dptr = valorFinal;
+#ifdef DEBUG
+			if(valorFinal >= valorMaximo) valorMaximo = valorFinal;
+			if(valorFinal <= valorMinimo) valorMinimo = valorFinal;
+#endif
+			dptr += 1;
+		}
+		dptr += ioffset;
+	}
+
+	//dptr = dst + offsetInicial;
+	dptr = bufferDeTrabajo + offsetInicial;
+//	uint8_t * maxc = dptr + columnas;
+	double * maxc = dptr + columnas;
+
+#ifdef TEST
+	fprintf(stderr, "Valor maximo: %f, Valor minimo: %f, Aplicando filtro vertical...\n", valorMaximo, valorMinimo);
+#endif
+
+	centroFiltro = vfiltro + foffset;
+	while(dptr < maxc)
+	{
+		//uint8_t * dptrc = dptr;
+		double * dptrc = dptr;
+		maxf = dptr + filas * width;
+		while(dptr < maxf)
+		{
+			//Aplicamos el filtro verticalmente
+			//uint8_t valorFinal = 0;
+			double valorFinal = 0;
+			int idx;
+			for(idx = -foffset; idx <= foffset; idx++)
+			{
+				valorFinal += *(dptr+idx*(int)width) * *(centroFiltro+idx);
+
+			}
+			*dptr = valorFinal;
+#ifdef TEST
+			if(valorFinal >= valorMaximo) valorMaximo = valorFinal;
+			if(valorFinal <= valorMinimo) valorMinimo = valorFinal;
+#endif
+			dptr += width;
+		}
+		dptr = dptrc + 1;
+	}
+
+#ifdef DEBUG
+	fprintf(stderr, "Valor maximo: %f, Valor minimo: %f\n", valorMaximo, valorMinimo);
+#endif
+	for(pos = 0; pos < size; pos++)
+	{
+		dst[pos] = (uint8_t) bufferDeTrabajo[pos];
+	}
+
+	free(bufferDeTrabajo);
+
+}
+
+void aplicarFiltro_Double(uint8_t * src, double * dst, unsigned int width, unsigned int height, float * hfiltro, float * vfiltro, unsigned int tamFiltro)
+{
+	unsigned int size = height * width;
+	
+	//Copiamos todo en dst
+	int pos;
+	for(pos = 0; pos < size; pos++)
+	{
+		dst[pos] = (double) src[pos];
+	}
+
+	int foffset = tamFiltro >> 1;
+	int ioffset = foffset << 1;
+
+
+	//Numero de filas y columnas de la zona central (descartamos bordes de la imagen del grosor del filtro)
+	int filas    = height - ioffset;
+	int columnas = width - ioffset;
+
+	
+	//Pasada por filas (aplicación del filtro horizontal)
+	unsigned int offsetInicial = foffset * width + foffset;
+
+	double * dptr;
+
+	dptr = dst + offsetInicial;
+
+	double * maxf = dptr + width * filas;
+	
+#ifdef DEBUG
+	fprintf(stderr, "Aplicando filtro horizontal...\n");
+	double valorMaximo = -9999, valorMinimo = 9999;
+#endif
+
+	float * centroFiltro = hfiltro + foffset;
+	
+	while(dptr < maxf)
+	{
+		double * maxc = dptr + columnas;
+		while(dptr < maxc)
+		{
+			//Aplicamos el filtro horizontalmente
+			double valorFinal = 0;
+			int idx;
+			for(idx = -foffset; idx <= foffset; idx++)
+			{
+				valorFinal += *(dptr+idx) * *(centroFiltro+idx);
+
+			}
+			*dptr = valorFinal;
+			dptr += 1;
+#ifdef DEBUG
+			if(valorFinal >= valorMaximo) valorMaximo = valorFinal;
+			if(valorFinal <= valorMinimo) valorMinimo = valorFinal;
+#endif
+
+		}
+		dptr += ioffset;
+	}
+
+//	return;
+	dptr = dst + offsetInicial;
+	double * maxc = dptr + columnas;
+
+	fprintf(stderr, "Aplicando filtro vertical...\n");
+
+	centroFiltro = vfiltro + foffset;
+	while(dptr < maxc)
+	{
+		double * dptrc = dptr;
+		maxf = dptr + filas * width;
+		while(dptr < maxf)
+		{
+			//Aplicamos el filtro verticalmente
+			double valorFinal = 0;
+			int idx;
+			for(idx = -foffset; idx <= foffset; idx++)
+			{
+				valorFinal += *(dptr+idx*(int)width) * *(centroFiltro+idx);
+
+			}
+			*dptr = valorFinal;
+			dptr += width;
+#ifdef DEBUG
+			if(valorFinal >= valorMaximo) valorMaximo = valorFinal;
+			if(valorFinal <= valorMinimo) valorMinimo = valorFinal;
+#endif
+
+		}
+		dptr = dptrc + 1;
+	}
+#ifdef DEBUG
+	fprintf(stderr, "Valor maximo: %f, Valor minimo: %f\n", valorMaximo, valorMinimo);
+#endif
+
+
+}
+void aplicarFiltro_Uint8(uint8_t * src, uint8_t * dst, unsigned int width, unsigned int height, float * hfiltro, float * vfiltro, unsigned int tamFiltro)
 {
 	unsigned int size = height * width;
 	
@@ -203,7 +413,6 @@ void filtrarRuido(uint8_t * src, uint8_t * dst, unsigned int width, unsigned int
 	int foffset = tamFiltro >> 1;
 	int ioffset = foffset << 1;
 
-	float * centroFiltro = filtro + foffset;
 
 	//Numero de filas y columnas de la zona central (descartamos bordes de la imagen del grosor del filtro)
 	int filas    = height - ioffset;
@@ -219,9 +428,12 @@ void filtrarRuido(uint8_t * src, uint8_t * dst, unsigned int width, unsigned int
 
 	uint8_t * maxf = dptr + width * filas;
 	
+#ifdef DEBUG
 	fprintf(stderr, "Aplicando filtro horizontal...\n");
+	double valorMaximo = -9999, valorMinimo = 9999;
+#endif
 
-//	fprintf(stderr, "Aplicando filtro horizontal...%d -- %d -- %d -- %d\n", foffset, ioffset, filas, columnas);
+	float * centroFiltro = hfiltro + foffset;
 	while(dptr < maxf)
 	{
 		uint8_t * maxc = dptr + columnas;
@@ -232,11 +444,16 @@ void filtrarRuido(uint8_t * src, uint8_t * dst, unsigned int width, unsigned int
 			int idx;
 			for(idx = -foffset; idx <= foffset; idx++)
 			{
-//			fprintf(stderr, "Algo passe %ld -- %d -- %d\n", (unsigned long )dptr,idx, foffset);
 				valorFinal += *(dptr+idx) * *(centroFiltro+idx);
 			}
 			*dptr = valorFinal;
 			dptr += 1;
+#ifdef DEBUG
+			if(valorFinal >= valorMaximo) valorMaximo = valorFinal;
+			if(valorFinal <= valorMinimo) valorMinimo = valorFinal;
+#endif
+
+
 		}
 		dptr += ioffset;
 	}
@@ -245,6 +462,8 @@ void filtrarRuido(uint8_t * src, uint8_t * dst, unsigned int width, unsigned int
 	uint8_t * maxc = dptr + columnas;
 
 	fprintf(stderr, "Aplicando filtro vertical...\n");
+
+	centroFiltro = vfiltro + foffset;
 	while(dptr < maxc)
 	{
 		uint8_t * dptrc = dptr;
@@ -257,18 +476,129 @@ void filtrarRuido(uint8_t * src, uint8_t * dst, unsigned int width, unsigned int
 			for(idx = -foffset; idx <= foffset; idx++)
 			{
 				valorFinal += *(dptr+idx*(int)width) * *(centroFiltro+idx);
-
 			}
 			*dptr = valorFinal;
 			dptr += width;
+#ifdef DEBUG
+			if(valorFinal >= valorMaximo) valorMaximo = valorFinal;
+			if(valorFinal <= valorMinimo) valorMinimo = valorFinal;
+#endif
 		}
 		dptr = dptrc + 1;
 	}
+#ifdef DEBUG
+	fprintf(stderr, "Valor maximo: %f, Valor minimo: %f\n", valorMaximo, valorMinimo);
+#endif
+
+
 }
 
-void showError()
+static aplicarFiltroNoSeparable(uint8_t * src, double * dst, unsigned int width, unsigned int height, double ** filtro, unsigned int tfiltro)
+{
+
+	unsigned int size = width * height;
+	uint8_t ** sM = (uint8_t**) malloc(height * sizeof(uint8_t*));
+	double ** dM = (double**) malloc(height * sizeof(double*));
+
+	uint8_t * sptr;
+	double * dptr;
+	int pos;
+
+	for(pos = 0, sptr = src, dptr = dst; pos < height; pos += 1, sptr += width, dptr += width)
+	{
+		sM[pos] = sptr;
+		dM[pos] = dptr;
+	}
+
+	unsigned int foffset = tfiltro >> 1;
+
+	int f, c;
+	for(f=foffset; f < height-foffset; f++)
+	{
+		for(c=foffset; c < width-foffset; c++)
+		{
+			int f2,c2, ff,fc, f2o = f-foffset, c2o = c-foffset;
+			double v = 0;
+			for(f2 = f2o, ff = 0; ff < tfiltro; f2++, ff++)
+			{
+				for(c2 = c2o, fc = 0; fc < tfiltro; c2++, fc++)
+				{
+					v += sM[f2][c2] * filtro[ff][fc];
+				}
+			}
+			dM[f][c] = v;
+		}
+	}
+
+}
+static void obtenerModuloGradiente(uint8_t * xg, uint8_t * yg, uint8_t * mg, unsigned int width, unsigned int height)
+{
+	unsigned int length = width * height;
+	uint8_t * xptr = xg, *yptr = yg, *mptr = mg;
+	uint8_t * maxxptr = xptr + width * height;
+
+	while(xptr < maxxptr)
+	{
+
+		*mptr = sqrt(*xptr**xptr +  *yptr**yptr);
+
+	//	fprintf(stderr, "XG: %d , XG: %d, MG = %d (%f)\n", *xptr, *yptr, *mptr,sqrt(*xptr**xptr +  *yptr**yptr));
+		mptr++;		
+		xptr++;
+		yptr++;
+	}
+}
+
+static void obtenerDireccionGradiente(uint8_t * xg, uint8_t * yg, uint8_t * mg, unsigned int width, unsigned int height)
+{
+
+}
+
+static void showError()
 {
 	fprintf(stderr, "Ha ocurrido algun error: %s\n", strerror(errno));
+}
+
+static void getM_B(double smax, double smin, double * m, double *b)
+{
+	*m = 255. / (smax - smin);
+	*b = - (smin * *m);
+}
+		
+static void escalar_Double_Uint8(double * ygradiente, uint8_t * ygescalado, unsigned int width, unsigned int height)
+{
+	unsigned int size = width * height;
+	double * sptr,
+	* maxsptr = ygradiente + size;
+
+	uint8_t * dptr,
+	* maxdptr = ygescalado + size;
+
+	double vmax = -9999, vmin = 9999;
+
+	for(sptr = ygradiente; sptr < maxsptr; sptr++)
+	{
+		if(*sptr <= vmin) vmin = *sptr;
+		if(*sptr >= vmax) vmax = *sptr;
+	}
+
+	double M, B;
+
+	getM_B(vmax, vmin, &M, &B);
+	fprintf(stderr, "Maximo: %f , Minimo: %f\n", vmax, vmin);
+
+	for(sptr = ygradiente, dptr = ygescalado; sptr < maxsptr; sptr++, dptr++)
+	{
+		*dptr = *sptr * M + B;	
+	}
+
+}
+
+static void saveImage(int fd, uint8_t * header, unsigned int hlength, uint8_t * content, unsigned int clength)
+{
+	int writen = write(fd, header, hlength);
+	writen += write(fd, content, clength);
+	fprintf(stderr, "Escritos: %d\n", writen);
 }
 
 int main(int argc, char ** argv)
@@ -290,7 +620,30 @@ int main(int argc, char ** argv)
 
 	fprintf(stderr, "Sigma: %f , Size: %d\n", sigma, tamFiltro);
 	showFilter(stderr, filtro, tamFiltro);
-	
+
+	int gsize = 3;
+
+	double ** Gx = (double **) malloc(gsize * sizeof(double*));
+
+	int f;
+	for(f = 0; f < gsize; f++)
+	{
+		Gx[f] = (double *) malloc(gsize* sizeof(double));
+	}
+
+	Gx[0][0] = -1; Gx[0][1] = 0; Gx[0][2] = 1;
+	Gx[1][0] = -2; Gx[1][1] = 0; Gx[1][2] = 2;
+	Gx[2][0] = -1; Gx[2][1] = 0; Gx[2][2] = 1;
+
+
+/*
+	float Gxh[3] = {-1, 0, 1};
+	float Gxv[3] = { 1, 2, 1};
+
+	float Gyh[3] = { 1, 2, 1};
+	float Gyv[3] = {-1, 0, 1};
+*/
+	unsigned int gSize = 3;
 	if(read_header(&width, &height) == 0)
 	{
 		fprintf(stderr, "Recibido ppm header\nEsperando RGB...\n");
@@ -318,9 +671,9 @@ int main(int argc, char ** argv)
 		rgb2gs(rgb, gs, width, height);
 
 
-		int fppm = open("salida.ppm", O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IROTH );
+		int fppm = open("original-color.ppm", O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IROTH );
 		if (fppm < 0) showError();
- 		int fpgm = open("salida.pgm", O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IROTH );
+ 		int fpgm = open("original-gs.pgm", O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IROTH );
 		if (fpgm < 0) showError();
 
 		int res = write(fppm, ppm, ppmLength);
@@ -332,12 +685,59 @@ int main(int argc, char ** argv)
 		close(fpgm);
 
 		unsigned char * gsFiltrado = (uint8_t*) malloc(pixelLength);
+		double * xgradiente = (double*) malloc(pixelLength * sizeof(double));
+		double * ygradiente = (double*) malloc(pixelLength * sizeof(double));
+		double * mgradiente = (double*) malloc(pixelLength * sizeof(double));
+		double * dgradiente = (double*) malloc(pixelLength * sizeof(double));
 
-		filtrarRuido(gs, gsFiltrado, width, height, filtro, tamFiltro);
+		//Filtramos el ruido con la gausiana especificada por el usuario
+		aplicarFiltro_Uint8_ConPrecision(gs, gsFiltrado, width, height, filtro, filtro, tamFiltro);
+		//aplicarFiltroConPrecision(gs, gsFiltrado, width, height, filtro, filtro, tamFiltro);
+	
+		//Obtenemos el cambio de intensidad del gradiente en X
+		//aplicarFiltro_Double(gsFiltrado, xgradiente, width, height, Gxh, Gxv, gSize);
+		aplicarFiltroNoSeparable(gsFiltrado, xgradiente, width, height, Gx, 3);
+		uint8_t * xgescalado = (uint8_t*) malloc(pixelLength);
+		escalar_Double_Uint8(xgradiente, xgescalado, width, height);
+		//aplicarFiltroConPrecision(gsFiltrado, xgradiente, width, height, Gxh, Gxv, gSize);
 
-		int writen = write(1, pgm, pgmhl);
-		writen += write(1, gsFiltrado, pixelLength);
-		fprintf(stderr, "Escritos: %d\n", writen);
+		//Obtenemos el cambio de intensidad del gradiente en Y
+		//aplicarFiltro_Double(gsFiltrado, ygradiente, width, height, Gyh, Gyv, gSize);
+		uint8_t * ygescalado = (uint8_t*) malloc(pixelLength);
+		//escalar_Double_Uint8(ygradiente, ygescalado, width, height);
+		//aplicarFiltroConPrecision(gsFiltrado, ygradiente, width, height, Gyh, Gyv, gSize);
+		
+		//Obtenemos el modulo del gradiente
+		//obtenerModuloGradiente(xgradiente, ygradiente, mgradiente, width, height);
+
+		//Obtenemos la direccion del gradiente
+		//obtenerDireccionGradiente(xgradiente, ygradiente, dgradiente, width, height);
+
+
+		int ffiltrado = open("01-filtrada.ppm", O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IROTH );
+		if (ffiltrado < 0) showError();
+ 		int fxgradiente = open("02-xgradiente.pgm", O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IROTH );
+		if (fxgradiente < 0) showError();
+		int fygradiente = open("03-ygradiente.pgm", O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IROTH );
+		if (fygradiente < 0) showError();
+		int fmgradiente = open("04-mgradiente.pgm", O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IROTH );
+		if (fmgradiente < 0) showError();
+		int fdgradiente = open("05-dgradiente.pgm", O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IROTH );
+		if (fdgradiente < 0) showError();
+
+	
+		saveImage(ffiltrado, pgm, pgmhl, gsFiltrado, pixelLength);
+		saveImage(fxgradiente, pgm, pgmhl, xgescalado, pixelLength);
+		saveImage(fygradiente, pgm, pgmhl, ygescalado, pixelLength);
+		//saveImage(fmgradiente, pgm, pgmhl, mgradiente, pixelLength);
+		//saveImage(fdgradiente, pgm, pgmhl, dgradiente, pixelLength);
+
+		close(ffiltrado);
+		close(fxgradiente);
+		close(fygradiente);
+		close(fmgradiente);
+		close(fdgradiente);
+
 
 
 
