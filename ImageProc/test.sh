c579a8026e2191a3cf81394226a021d35b2dda95
Diego Centelles <dieguscentbelt@gmail.com>
2016-02-15 10:19:29 +0100

    Anadida version para temporizar cada paso

--- ImageProc/test.sh
+++ ImageProc/test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-./join -a 1000 -l 5 -s 1 -U 150 -L 50 -e < noise2.ppm > acc.pgm
+##./arm-join-v1 -a 1000 -l 5 -s 1 -U 150 -L 50 -e < noise2.ppm > acc.pgm
+
+ffmpeg -f video4linux2 -s 1280x720 -i /dev/video0 -f image2pipe -vframes 1 -vcodec ppm pipe:1 | ./arm-join-v1 -a 1000 -l 3 -s 0.5 -U 50 -L 20 -e > acc.pgm
