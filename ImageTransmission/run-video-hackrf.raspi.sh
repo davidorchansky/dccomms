9516c7dc0cd662b8e59747dbec843dddbf084879
didaccb <centelld@uji.es>
2016-05-19 07:00:51 +0000

    anadidos scripts para poner en marcha y parar la transmission con hackrf y radiometrix para el sistema de la raspberry

---
+++ ImageTransmission/run-video-hackrf.raspi.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/home/alarm/ROVRadioTransmission/ImageTransmission/bin/TX_hackrf -W 1280 -H 720 -I imagen -F 1400 -L 4000 -D 200 -A 0 -P /home/alarm/txrf
+
---
+++ ImageTransmission/stop-video-hackrf.raspi.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pkill run-video-hackrf.raspi.sh
