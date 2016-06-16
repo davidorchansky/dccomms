b15ed43eb27d747f98c2b87a470a4c18c655160e
didaccb <centelld@uji.es>
2016-06-16 22:55:00 +0000

    anadidos ficheros de testeo tiempos canny en opencv

--- ImageProc/cannycv.cpp
+++ ImageProc/cannycv.cpp
@@ -1,24 +1,26 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgcodecs.hpp"
 #include <stdlib.h>
 #include <stdio.h>
 #include <cstdio>
 #include <sys/time.h>
 #include <iostream>
 using namespace cv;
-
+using namespace std;
 /// Global variables
 
 Mat src, src_gray;
-Mat dst, detected_edges;
+Mat dst, detected_edges, nnoise;
+
 
 int edgeThresh = 1;
-int lowThreshold;
+int lowThreshold = 40;
 int const max_lowThreshold = 100;
 int ratio = 3;
 int kernel_size = 3;
-char* window_name = "Edge Map";
 
+struct timeval t0,t1;
 /**
  * @function CannyThreshold
  * @brief Trackbar callback - Canny thresholds input with a ratio 1:3
@@ -35,30 +37,36 @@ static void mostrarTiempo(const char * info, struct timeval *t0, struct timeval 
 void CannyThreshold(int, void*)
 {
 
-struct timeval t0,t1;
-  gettimeofday(&t0, NULL);
   /// Reduce noise with a kernel 3x3
-  blur( src_gray, detected_edges, Size(5,5) );
 
-  /// Canny detector
-  Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
-
+  gettimeofday(&t0, NULL);
+  GaussianBlur( src_gray, nnoise, Size(5,5),1 ,1);
   gettimeofday(&t1, NULL);
-	mostrarTiempo("Elapsed: ",&t0,&t1); 
+  mostrarTiempo("Noise",&t0,&t1); 
+
+  gettimeofday(&t0, NULL);
+  /// Canny detector
+  Canny( nnoise, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
+  gettimeofday(&t1, NULL);
+  mostrarTiempo("Canny",&t0,&t1); 
 
   /// Using Canny's output as a mask, we display our result
-  dst = Scalar::all(0);
+  //dst = Scalar::all(0);
 
-  src.copyTo( dst, detected_edges);
-  imshow( window_name, dst );
+  //src.copyTo( dst, detected_edges);
+  //imshow( window_name, dst );
  }
 
 
 /** @function main */
 int main( int argc, char** argv )
 {
+
+  gettimeofday(&t0, NULL);
   /// Load an image
   src = imread( argv[1] );
+  gettimeofday(&t1, NULL);
+  mostrarTiempo("Leer imagen",&t0,&t1); 
 
   if( !src.data )
   { return -1; }
@@ -66,20 +74,17 @@ int main( int argc, char** argv )
   /// Create a matrix of the same type and size as src (for dst)
   dst.create( src.size(), src.type() );
 
+  gettimeofday(&t0, NULL);
   /// Convert the image to grayscale
   cvtColor( src, src_gray, CV_BGR2GRAY );
+  gettimeofday(&t1, NULL);
+  mostrarTiempo("Convertir a GS",&t0,&t1); 
 
-  /// Create a window
-  namedWindow( window_name, CV_WINDOW_AUTOSIZE );
-
-  /// Create a Trackbar for user to enter threshold
-  createTrackbar( "Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold );
 
   /// Show the image
   CannyThreshold(0, 0);
 
-  /// Wait until user exit program by pressing a key
-  waitKey(0);
-
+  imwrite("no-noise.pgm",nnoise);
+  imwrite("edges.pgm", detected_edges);
   return 0;
-  }
+ }
