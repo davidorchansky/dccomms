ec3a3818eff2a93c2b95c6bb1b8c54f44b5dbab7
Diego Centelles <dieguscentbelt@gmail.com>
2016-09-19 09:37:50 +0200

    eliminado proyecto de implementaciones del algoritmo de Canny y la transformada de Hough. Este proyecto se ha trasladado a un repositorio dedicado: https://github.com/didaccb/canny-hough.git

--- ImageProc/tests/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-dir=../
-##./arm-join-v1 -a 1000 -l 5 -s 1 -U 150 -L 50 -e < noise2.ppm > acc.pgm
-
-ffmpeg -f video4linux2 -s 1280x720 -i /dev/video0 -f image2pipe -vframes 1 -vcodec ppm pipe:1 | $dir/arm-join-v1 -a 1000 -l 5 -s 0.5 -U 50 -L 20 -e > acc.pgm
