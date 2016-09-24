b3261c7d0bb0cf43ebcae6d9d37e95f2b5409000
Diego Centelles <dieguscentbelt@gmail.com>
2016-09-24 13:52:42 +0200

    estado actual

--- includes/Utils.h
+++ includes/Utils.h
@@ -8,7 +8,14 @@
 #ifndef UTILS_H_
 #define UTILS_H_
 #include <cstdint>
+#include <initializer_list>
+#include <string>
+#include <iostream>
+
 namespace radiotransmission {
+
+
+#define LOG_DEBUG(msg) std::cerr << "DEBUG("+std::string(typeid(*this).name())+":"+std::string(__func__)+"): " + std::string(msg) << std::endl; std::cerr << std::flush;
 
 class Utils {
 public:
@@ -16,6 +23,8 @@ public:
 	virtual ~Utils();
 	static bool IsBigEndian();
 	static void IntSwitchEndian(void * b, uint32_t entero);
+	static std::string BuildString(std::initializer_list<std::string> list );
+	static void Debug(std::ostream &, std::string &);
 };
 
 } /* namespace radiotransmission */
