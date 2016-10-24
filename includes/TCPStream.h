091f83858236da152c76b23c3652a3cda480d9aa
Diego Centelles <dieguscentbelt@gmail.com>
2016-10-24 15:59:11 +0200

    testing TCPStream as ICommsDevice

---
+++ includes/TCPStream.h
@@ -0,0 +1,58 @@
+/*
+ * TCPStream.h
+ *
+ *  Created on: 24 oct. 2016
+ *      Author: diego
+ */
+
+#ifndef INCLUDES_TCPSTREAM_H_
+#define INCLUDES_TCPSTREAM_H_
+
+#include <ICommsDevice.h>
+#include <string>
+
+//Cstyle
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+namespace dccomms {
+class TCPStream : public ICommsDevice {
+public:
+	TCPStream(std::string serveraddr);
+	virtual ~TCPStream();
+	void CloseConnection();
+	void OpenConnection();
+
+	int Read(void *, uint32_t, unsigned long msTimeout=0);
+	int Write(const void *, uint32_t, uint32_t msTimeout=0);
+
+	IStream & operator >> (uint8_t &);
+	IStream & operator >> (char &);
+	IStream & operator >> (uint16_t &);
+	IStream & operator >> (uint32_t &) ;
+
+	int Available();
+
+	bool IsOpen();
+	//void TimeoutMode(bool);
+	void FlushInput();
+	void FlushOutput();
+	void FlushIO();
+
+	bool Open();
+	void Close();
+private:
+	int sockfd, portno;
+	std::string ip;
+	struct sockaddr_in device_addr;
+	struct hostent *device;
+
+};
+}
+#endif /* INCLUDES_TCPSTREAM_H_ */
